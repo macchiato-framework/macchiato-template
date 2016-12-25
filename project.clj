4c2d2ec7194c1e96b334faf5c819f44e29fa90fe
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2016-12-25 15:30:24 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/lein-template "0.2.2"
+(defproject macchiato/lein-template "0.2.3"
   :description "A Leiningen template for creating express.js based ClojureScript apps."
   :url "https://github.com/macchiato-framework/macchiato-template"
   :license {:name "MIT License"
--- resources/leiningen/new/macchiato/src/middleware.cljs
+++ resources/leiningen/new/macchiato/src/middleware.cljs
@@ -1,6 +1,6 @@
 (ns {{project-ns}}.middleware
   (:require
-    [macchiato.defaults :as defaults]))
+    [macchiato.middleware.defaults :as defaults]))
 
 (defn wrap-defaults [handler]
   (defaults/wrap-defaults handler defaults/site-defaults))

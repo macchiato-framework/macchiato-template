893dd66cba117d5f91509043f6fbc1343b44f6f5
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-03-20 09:22:42 -0400

    bumped up lein-npm

--- resources/leiningen/new/macchiato/project.clj
+++ resources/leiningen/new/macchiato/project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/clojurescript "1.9.473"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-doo "0.1.7"]
-            [macchiato/lein-npm "0.6.2"]
+            [macchiato/lein-npm "0.6.3"]
             [lein-figwheel "0.5.9"]
             [lein-cljsbuild "1.1.4"]]
   :npm {:dependencies [[source-map-support "0.4.6"]]

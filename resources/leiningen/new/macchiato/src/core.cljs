9a8977cadb1ed515c1f186d1f89f7c88f56a3a5e
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-05-13 11:14:52 -0400

    bumped up to latest macchiato

--- resources/leiningen/new/macchiato/src/core.cljs
+++ resources/leiningen/new/macchiato/src/core.cljs
@@ -4,7 +4,7 @@
     [{{project-ns}}.middleware :refer [wrap-defaults]]
     [{{project-ns}}.routes :refer [router]]
     [macchiato.server :as http]
-    [macchiato.session.memory :as mem]
+    [macchiato.middleware.session.memory :as mem]
     [mount.core :as mount :refer [defstate]]
     [taoensso.timbre :refer-macros [log trace debug info warn error fatal]]))
 

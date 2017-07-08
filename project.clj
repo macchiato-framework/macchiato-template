4f75c06222d54d78d342eaac0e24ca4f066e234e
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-07-08 17:23:28 -0400

    bumped up macchiato-core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/lein-template "0.2.26"
+(defproject macchiato/lein-template "0.2.27"
   :description "A Leiningen template for creating Node.js based ClojureScript apps."
   :url "https://github.com/macchiato-framework/macchiato-template"
   :license {:name "MIT License"
--- resources/leiningen/new/macchiato/project.clj
+++ resources/leiningen/new/macchiato/project.clj
@@ -5,7 +5,7 @@
                  [com.cemerick/piggieback "0.2.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [hiccups "0.3.0"]
-                 [macchiato/core "0.1.9"]
+                 [macchiato/core "0.2.0"]
                  [macchiato/env "0.0.6"]
                  [mount "0.1.11"]
                  [org.clojure/clojure "1.8.0"]

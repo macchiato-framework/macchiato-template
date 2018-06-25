27337323d25d8ec2d1e866a638cd979f8c3c82c3
Sotnikov, Dmitri <dmitri.sotnikov@uhn.ca>
2018-06-25 15:04:01 -0400

    bumped up cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/lein-template "0.2.53"
+(defproject macchiato/lein-template "0.2.54"
   :description "A Leiningen template for creating Node.js based ClojureScript apps."
   :url "https://github.com/macchiato-framework/macchiato-template"
   :license {:name "MIT License"

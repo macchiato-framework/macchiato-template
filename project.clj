9d80c217a3dd5a2ea6c718904d3e8f92dea16c97
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-04-01 17:05:42 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/lein-template "0.2.60"
+(defproject macchiato/lein-template "0.2.61"
   :description "A Leiningen template for creating Node.js based ClojureScript apps."
   :url "https://github.com/macchiato-framework/macchiato-template"
   :license {:name "MIT License"

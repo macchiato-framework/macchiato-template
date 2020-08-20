a1e76e40ba1e9867db14f6f11b91965803bb29ff
Dmitri Sotnikov <yogthos@pop-os.localdomain>
2020-08-20 19:28:00 -0400

    bumped up macchiato-core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/lein-template "0.2.62"
+(defproject macchiato/lein-template "0.2.63"
   :description "A Leiningen template for creating Node.js based ClojureScript apps."
   :url "https://github.com/macchiato-framework/macchiato-template"
   :license {:name "MIT License"
